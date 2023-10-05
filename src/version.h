044bb74900796f615b81fb29ebe9e33dec7de07d
phaistra <markyxr@yahoo.com>
2023-10-04 22:12:58 -0400

    Update cmakeLists.txt

---
+++ src/version.h
@@ -0,0 +1,170 @@
+/* **************************************
+	File Name: Generated version Info
+	File Generated: 2023-10-04 : 22:12:45
+*************************************** */
+#ifndef _VERSION_INFO_H_
+#define _VERSION_INFO_H_
+
+/* **************************************
+	#defines
+*************************************** */
+
+
+/* **************************************
+	Declarations
+*************************************** */
+#define APP_NAME "cMakeStarter"
+
+
+/* SW Version */
+#define RELEASE_VERSION "1.0.0.0"
+#define MAJOR_VERSION 1
+#define MINOR_VERSION 0
+#define PATCH_VERSION 0
+#define REVISION 0
+
+/* Git Tags */
+#define GIT_BUILD_VERSION 8174584
+#define GIT_BUILD_VERSION_TAG "8174584"
+#define GIT_BUILD_BRANCH_NAME "Updated_cmake"
+
+#define ARCH Windows-10.0.19044
+#define COMP AMD64
+#define BUILD  
+
+/* Data about system which compiled this file */
+#define ARCH Windows-10.0.19044
+#define COMP AMD64
+#define BUILD_TYPE  
+#define BUILD_ROOT_DIR "I:/DEV/DSKTP/CPP/cMakeStarter"
+
+/* CMake Data */
+#define APP_BUILD_WITH_CMAKE_VERSION "3.26.4-msvc4"
+#define COMPILER "MSVC"
+#define CMAKE_COMPILER_VERSION "19.37.32822.0"
+#define APP_BUILD_ON_PLATFORM "Windows-10.0.19044"
+
+/* Info about the person who generated this SW*/
+#define BUILDER_USER_DOMAIN "MRIG"
+#define BUILDER_USER_COMPUTER_NAME "MRIG\Markus"
+
+#define ROOT "I:/DEV/DSKTP/CPP/cMakeStarter"
+/* ********************************************************
+
+	CMAKE_CXX_FLAGS_DEBUG = /MDd /Zi /Ob0 /Od /RTC1 /MDd
+	CMAKE_CXX_FLAGS_RELEASE = /MT /O2 /Ob2 /DNDEBUG /MT
+
+
+
+	Build Stuff
+	===========
+	CMAKE_C_FLAGS         = /DWIN32 /D_WINDOWS /W3
+	CMAKE_C_FLAGS_DEBUG   = /MDd /Zi /Ob0 /Od /RTC1
+	CMAKE_C_FLAGS_RELEASE = /MD /O2 /Ob2 /DNDEBUG
+
+	CMAKE_CXX_FLAGS         = /DWIN32 /D_WINDOWS /W3 /GR /EHsc
+	CMAKE_CXX_FLAGS_DEBUG   = /MDd /Zi /Ob0 /Od /RTC1 /MDd
+	CMAKE_CXX_FLAGS_RELEASE = /MT /O2 /Ob2 /DNDEBUG /MT
+
+	CMAKE_EXE_LINKER_FLAGS_DEBUG
+	============================
+	/debug /INCREMENTAL
+
+	CMAKE_EXE_LINKER_FLAGS_RELEASE
+	==============================
+	/INCREMENTAL:NO
+
+	CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO
+	=====================================
+	/debug /INCREMENTAL
+
+	CMAKE_EXE_LINKER_FLAGS_MINSIZEREL
+	=================================
+	/INCREMENTAL:NO
+
+
+
+	COMPILE_DEFINITIONS = 
+
+	SOURCE_FILES_PRINT
+	======
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/app/app.cpp;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/exampleCode/examples.cpp;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/globals/logger.cpp;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/main/main.cpp;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/main/mainLoop.cpp;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/threadCtrl/thread_ctrl.cpp;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/utilities/timestamp.cpp
+
+	HEADER_FILES_PRINT
+	======
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/app/app.h;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/exampleCode/examples.h;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/globals/events.h;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/globals/globals.h;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/globals/logger.h;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/main/mainLoop.h;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/main/version.h;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/threadCtrl/thread_ctrl.h;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/utilities/bitManipulation.h;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/utilities/enumerator.h;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/utilities/randomNumber.h;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/utilities/result.h;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/utilities/ring.hpp;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/utilities/ring_span.hpp;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/utilities/subprocess.hpp;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/utilities/timestamp.h;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/utilities/utilities.h;
+	I:/DEV/DSKTP/CPP/cMakeStarter/src/version.h
+
+	VENDOR INC DIRS
+	======
+	vendor/asyncpp;
+	vendor/concurrentqueue;
+	vendor/copper;
+	vendor/duckdb;
+	vendor/dylib;
+	vendor/hashlib;
+	vendor/json;
+	vendor/magic_enum;
+	vendor/parallel_hashmap;
+	vendor/spdlog;
+	vendor/sqlite342;
+	vendor/tracy;
+	vendor/asyncpp/async++;
+	vendor/asyncpp/src;
+	vendor/concurrentqueue/build;
+	vendor/concurrentqueue/c_api;
+	vendor/concurrentqueue/internal;
+	vendor/concurrentqueue/build/msvc11;
+	vendor/concurrentqueue/build/msvc12;
+	vendor/concurrentqueue/build/msvc14;
+	vendor/concurrentqueue/build/msvc15;
+	vendor/concurrentqueue/build/msvc16;
+	vendor/dylib/.github;
+	vendor/dylib/example;
+	vendor/dylib/include;
+	vendor/dylib/tests;
+	vendor/dylib/.github/ISSUE_TEMPLATE;
+	vendor/dylib/.github/workflows;
+	vendor/hashlib/tests;
+	vendor/spdlog/cfg;
+	vendor/spdlog/details;
+	vendor/spdlog/fmt;
+	vendor/spdlog/sinks;
+	vendor/spdlog/fmt/bundled;
+	vendor/tracy/client;
+	vendor/tracy/common;
+	vendor/tracy/libbacktrace;
+	vendor/tracy/tracy
+
+	bleh
+	======
+	
+******************************************************** */
+/* **************************************
+	Extras
+*************************************** */
+
+
+#endif
---
+++ support/commands.ps1
@@ -0,0 +1,151 @@
+
+function generate_ninja_makefiles(){
+	Write-host "`n`nGenerating Ninja build files..." -f Blue
+	$Env:PATH += ";$pwd"
+	$Env:PATH += ";$pwd/support"
+	# cmake -DCMAKE_TOOLCHAIN_FILE="./support/toolchain/tc39xx-tasking.cmake" -G "Ninja Multi-Config" .
+	$cmd = ("cmake -DCMAKE_TOOLCHAIN_FILE=`"./support/toolchain/tc39xx-tasking.cmake`" -G `"Ninja Multi-Config`" .")
+	# Write-Host "cmd:" $cmd
+	# Write-Host "Env:PATH:" $Env:PATH
+	Invoke-Expression "& $cmd | out-null"
+	Write-host "Done!`n" -f Green
+}
+
+function fmt(){
+	Write-host "`n`nRunning code formatter..." -f Blue
+	# Write-Host "InvocationName:" $MyInvocation.InvocationName -f Yellow
+	# Write-Host "Script:" $PSCommandPath -f Yellow
+	# Write-Host "Path:" $PSScriptRoot -f Yellow
+	# Write-Host "Path:" $pwd -f Yellow
+	$Env:PATH += ";$pwd"
+	$Env:PATH += ";$pwd/support"
+	$cmd = (($PSScriptRoot) + "\clang-format-all.ps1 -RepoRoot 'src' -Include '*.h', '*.c' -Exclude '*.g.*'")
+	Invoke-Expression "& $cmd | out-null"
+	Write-host "Code format done!`n" -f Green
+}
+
+function cleanProject() {
+	$Directories = "bin", "build", "support/psClang", "psClang", "CMakeFiles", "CMakeCache.txt", "ninja.log", "cmake_install.cmake", ".ninja_log", ".ninja_deps", ".cache", "lib", "build.ninja", "build-Debug.ninja", "build-Release.ninja", "build-RelWithDebInfo.ninja"
+ 
+	#Delete files in each directory
+	ForEach ($Dir in $Directories) {
+		
+		If (Test-Path $Dir) {
+			# Folder not exist, delete it!
+			Remove-Item -Path $Dir -Recurse -Force
+			Write-host "'$Dir'" -f Yellow -NoNewline
+			Write-host " Deleted" -f Green
+		}
+		Else {
+			Write-host "'$Dir'" -f Yellow -NoNewline
+			Write-host " already deleted!" -f Green
+		}
+	}
+	
+	#Get-ChildItem *.sln | foreach { Remove-Item -Path $_.FullName }
+	#Get-ChildItem *.vcxproj | foreach { Remove-Item -Path $_.FullName }
+	#Get-ChildItem *.filters | foreach { Remove-Item -Path $_.FullName }
+	#Get-ChildItem lib\*.lib | foreach { Remove-Item -Path $_.FullName }
+	Write-host "Succesfully cleaned" -f Green
+
+}
+
+
+function exportSourcesOnly(){
+	$theDate = Get-Date -Format "MM.dd.yyyy - hh.mm.ss tt"
+	$export_name = "[" + $theDate + "] ARIES II Source.zip"
+	$archiveList = ".\src", ".\vendor", ".\support", ".\.vscode", ".\.clang-format", ".\.gitignore", ".\CMakeLists.txt", ".\clang-build.ps1", ".\justfile", ".\readme.md"
+	$all_files_present = 1;
+	$missingItem = 'None';
+
+	# Ensure neccessary files/folders are present
+	ForEach ($item in $archiveList) {
+		
+		If (Test-Path $item) {
+			# Write-host "'$item'" -f Yellow -NoNewline
+			# Write-host " present" -f Green
+		}
+		Else {
+			Write-host "'$item'" -f Yellow -NoNewline
+			Write-host " is missing!" -f Red
+			$missingItem = $item
+			$all_files_present = false
+			#break
+		}
+	}
+	If($all_files_present) {
+		$compress = @{
+			Path             = $archiveList
+			CompressionLevel = "Fastest"
+			DestinationPath  = $export_name
+		}
+		Compress-Archive @compress -Force
+		Write-host "Succesfully exported '$export_name'" -f Green
+	} else {
+		Write-host "Failed to export, '$missingItem' is missing!" -f Red
+	}
+	
+	
+	# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.archive/compress-archive?view=powershell-7.3
+}
+
+function exportAll(){
+	$theDate = Get-Date -Format "MM.dd.yyyy - hh.mm.ss tt"
+	$export_name = "[" + $theDate + "] ARIES II Project Data.zip"
+	$archiveList = ".\src", ".\vendor", ".\support",".\bin", ".\.vscode", ".\.clang-format", ".\.gitignore", ".\CMakeLists.txt", ".\clang-build.ps1", ".\justfile", ".\readme.md"
+	$all_files_present = 1
+	$missingItem = 'None'
+
+	# Ensure neccessary files/folders are present
+	ForEach ($item in $archiveList) {
+		If (Test-Path $item) {
+			# Write-host "'$item'" -f Yellow -NoNewline
+			# Write-host " present" -f Green
+		}
+		Else {
+			Write-host "'$item'" -f Yellow -NoNewline
+			Write-host " is missing!" -f Red
+			$missingItem = $item
+			$all_files_present = false
+			break
+		}
+	}
+
+	if ($all_files_present) {
+		$compress = @{
+			Path             = $archiveList
+			CompressionLevel = "Fastest"
+			DestinationPath  = $export_name
+		}
+		Compress-Archive @compress -Force
+		Write-host "Succesfully exported '$export_name'" -f Green
+	} else {
+		Write-host "Failed to export, '$missingItem' is missing!" -f Red
+	}
+}
+
+function windowsTerminal(){
+	#@%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe -d %cd%
+	Write-host "Launched windows terminal" -f Green	
+}
+
+# TEST FUNCTIONS
+function Add-Path($Path) {
+	$Path = [Environment]::GetEnvironmentVariable("PATH", "Machine") + [IO.Path]::PathSeparator + $Path
+	[Environment]::SetEnvironmentVariable( "Path", $Path, "Machine" )
+}
+
+# From ./support folder, run following pecific function like so:
+# powershell -command "& { . .\addPath.ps1; printSomething }"
+function printSomething() {
+	Write-host "PRINT SOMETHING TEST FUNCTION" -f Blue
+	Write-host "USER DOMAIN: $Env:UserDomain"
+	Write-host "COMPUTER NAME: $Env:ComputerName"
+
+}
+
+# Reference material
+# https://stackoverflow.com/questions/1405750/calling-a-specific-powershell-function-from-the-command-line
+# https://stackoverflow.com/questions/12850487/invoke-a-second-script-with-arguments-from-a-script
+# https://devblogs.microsoft.com/cppblog/finding-the-visual-c-compiler-tools-in-visual-studio-2017/
+# # https://superuser.com/questions/1091344/powershell-to-delete-all-files-with-a-certain-file-extension
